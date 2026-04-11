c501010a0ca1587e9327b19a7651084bfebdfb3e
Ali Ashraf 777 <aliiashraf777@gmail.com>
2026-04-11 18:03:47 +0500

    feat: complete remaining project

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import { headerLogo } from '../assets/images';
 import { hamburger } from '../assets/icons';
 import { navLinks } from '../constants';
-
+ 
 const Nav = () => {
     return (
         <header className='padding-x py-8 absolte z-10 w-full'>
