8d3fa35c5e326b92e4994ebd74ea95e2e47a1088
Ali Ashraf 777 <aliiashraf777@gmail.com>
2026-04-11 18:02:07 +0500

    feat(footer): add footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 import { copyrightSign } from '../assets/icons'
 import { footerLogo } from '../assets/images'
 import { footerLinks, socialMedia } from '../constants'
-
+ 
 const Footer = () => {
   return (
     <footer className='max-container'>
